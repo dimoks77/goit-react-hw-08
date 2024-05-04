2e484221e31df379fdc7194a723a629af627a73e
Dmytro Kasko <dimoks77@gmail.com>
2024-05-05 01:38:52 +0200

    autocomplete=on

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -50,7 +50,7 @@ export const LoginForm = () => {
           </div>
           <div className={css.label}>
             <label>Password</label>
-            <Field type="password" name="password" autocomplete="on" />
+            <Field type="password" name="password" autoComplete="on" />
             <ErrorMessage name="password" component="div" className={css.error} />
           </div>
           <button className={css.button} type="submit">
