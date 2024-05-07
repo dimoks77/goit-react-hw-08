4afe0abc869ed385f450375375c57a782940c025
Dmytro Kasko <dimoks77@gmail.com>
2024-05-07 19:33:09 +0200

    redesign 6

--- src/components/RegistrationForm/RegistrationForm.jsx
+++ src/components/RegistrationForm/RegistrationForm.jsx
@@ -5,6 +5,9 @@ import css from "./RegistrationForm.module.css";
 import { Button, TextField } from "@mui/material";
 import { InputAdornment, IconButton } from "@mui/material";
 import { Visibility, VisibilityOff } from "@mui/icons-material";
+import KeyIcon from '@mui/icons-material/Key';
+// import generatePassword from 'password-generator';
+
 
 export const RegistrationForm = () => {
   const dispatch = useDispatch();
@@ -17,6 +20,18 @@ export const RegistrationForm = () => {
 
   const [showPassword, setShowPassword] = useState(false);
   const handleClickShowPassword = () => setShowPassword((show) => !show);
+
+  const generateNewPassword = () => {
+    const chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    let newPassword = "";
+    for (let i = 0; i < 10; i++) {
+      newPassword += chars.charAt(Math.floor(Math.random() * chars.length));
+    }
+    setValues(prevValues => ({
+      ...prevValues,
+      password: newPassword 
+    }));
+};
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -37,7 +52,6 @@ export const RegistrationForm = () => {
       <h1 className={css.title}>Registration form</h1>
       <form className={css.form} autoComplete="off" onSubmit={handleSubmit}>
         <div className={css.label}>
-          {/* <label htmlFor="name">Name:</label> */}
           <TextField
             label="Name"
             type="text"
@@ -149,22 +163,40 @@ export const RegistrationForm = () => {
             }}
             InputProps={{
               endAdornment: (
-                <InputAdornment position="end">
-                  <IconButton
-                    aria-label="toggle password visibility"
-                    onClick={handleClickShowPassword}
-                    edge="end"
-                    sx={{
-                      border: 0,
-                      color: "#1976d2",
-                      '&:focus': {
-                        outline: 'none',
-                      },
-                    }}
-                  >
-                    {showPassword ? <VisibilityOff /> : <Visibility />}
-                  </IconButton>
-                </InputAdornment>
+                <>
+                  <InputAdornment position="end">
+                    <IconButton
+                      aria-label="toggle password visibility"
+                      onClick={handleClickShowPassword}
+                      edge="end"
+                      sx={{
+                        border: 0,
+                        color: "#1976d2",
+                        '&:focus': {
+                          outline: 'none',
+                        },
+                      }}
+                    >
+                      {showPassword ? <VisibilityOff /> : <Visibility />}
+                    </IconButton>
+                  </InputAdornment>
+                  <InputAdornment position="end">
+                    <IconButton
+                      aria-label="generate password"
+                      onClick={generateNewPassword}
+                      edge="end"
+                      sx={{
+                        border: 0,
+                        color: "#1976d2",
+                        '&:focus': {
+                          outline: 'none',
+                        },
+                      }}
+                    >
+                      <KeyIcon />
+                    </IconButton>
+                  </InputAdornment>
+                </>
               ),
             }}
           />
